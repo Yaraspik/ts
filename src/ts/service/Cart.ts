775915f04e165734388b77501eb30889eb6e3aff
Stepan Mareev <yaraspik@ya.ru>
2023-06-25 07:54:10 +0300

    Refactoring

--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -1,47 +1,36 @@
-import Buyable from '../domain/Buyable';
-import CartItem from '../components/CartItem';
-
-export default class Cart {
-    private _items: CartItem[] = [];
-
-    add(item: Buyable, count: number = 1): void {
-        const exist = this._items.find(cartItem => cartItem.item.id === item.id);
-        if(!exist) {
-            this._items.push(new CartItem(item, count));
-            return;
-        }
-        this._items = this._items.map(cartItem => {
-            return new CartItem(item, cartItem.count + count)
-        });
-    }
-
-    get items(): CartItem[] {
-        return [...this._items]; 
-    }
-
-    totalCost(): number {
-        let totalCost = 0;
-        const items = this._items;
-        items.forEach(element => {
-            totalCost += element.item.price;
-        });
-        return totalCost;
-    }
-
-    totalDiscountCost(discount: number): number {
-        let totalCost = 0;
-        const items = this._items;
-        items.forEach(element => {
-            totalCost += element.item.price;
-        });
-        return totalCost -= totalCost * (discount / 100) ;
-    }
-
-    removeItem(id: number): boolean {
-        const item = this._items.findIndex((el) => {
-            el.item.id === id;
-        });
-        this._items.splice(item, 1);
-        return true;
-    }
+import Buyable from '../domain/Buyable'; 
+import CartItem from '../components/CartItem'; 
+ 
+export default class Cart { 
+    private _items: CartItem[] = []; 
+ 
+    add(item: Buyable, count: number = 1): void { 
+        const exist = this._items.find(cartItem => cartItem.item.id === item.id); 
+        if(!exist) { 
+            this._items.push(new CartItem(item, count)); 
+            return; 
+        } 
+        this._items = this._items.map(cartItem => { 
+            return new CartItem(item, cartItem.count + count) 
+        }); 
+    } 
+ 
+    get items(): CartItem[] { 
+        return [...this._items];  
+    } 
+ 
+    totalCost(): number { 
+        return this._items.reduce((acc, el) => { 
+            return acc += el.item.price; 
+        }, 0); 
+    } 
+ 
+    totalDiscountCost(discount: number): number { 
+        let totalCost = this.totalCost(); 
+        return totalCost -= totalCost * (discount / 100) ; 
+    } 
+ 
+    removeItem(id: number): void { 
+        this._items = this._items.filter((el: CartItem) => el.item.id !== id); 
+    } 
 }
